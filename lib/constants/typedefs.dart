53a260b68b4223af1210056498e768ca78dc2e8e
Mohammad <mohammadhprp@gmail.com>
2023-07-08 12:30:47 +0330

    feat: create typedefs file

---
+++ lib/constants/typedefs.dart
@@ -0,0 +1,8 @@
+typedef CloseLoadingScreen = bool Function();
+typedef UpdateLoadingScreen = bool Function(String text);
+
+typedef Token = String;
+
+typedef FilePath = String;
+
+typedef IsLoading = bool;
