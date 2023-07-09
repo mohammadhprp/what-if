68b354ddaa14798b9b01994027457c32d5db6bd6
Mohammad <mohammadhprp@gmail.com>
2023-07-09 16:06:43 +0330

    feat: run test before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-git push --follow-tags --force
+# Run flutter test
+flutter test
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    # Push changes to GitHub 
+    git push --follow-tags --force
+
+fi
