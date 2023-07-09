94d82893695af9b3cff4b4e3c57224443da69bc5
Mohammad <mohammadhprp@gmail.com>
2023-07-09 15:30:20 +0330

    feat: design an auth background for login and register forms

---
+++ lib/widgets/auth/auth_background.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/app/app_colors.dart';
+import '../../constants/extensions/media_query/media_query_extension.dart';
+import '../../constants/values_manager/values_manager.dart';
+
+class AuthBackground extends StatelessWidget {
+  final Widget content;
+  const AuthBackground({super.key, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(AppPadding.p18),
+      width: context.width,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(AppSize.s12),
+          topRight: Radius.circular(AppSize.s12),
+        ),
+        color: AppColors.grey08,
+      ),
+      child: content,
+    );
+  }
+}
