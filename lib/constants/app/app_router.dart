37449dff703c616cd5b52e42b2cd2a1aa3288012
Mohammad <mohammadhprp@gmail.com>
2023-07-09 15:32:49 +0330

    feat: add Login and Register screen

--- lib/constants/app/app_router.dart
+++ lib/constants/app/app_router.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../screens/auth/auth_screen.dart';
+import '../../screens/auth/login_screen.dart';
+import '../../screens/auth/register_screen.dart';
+import '../../screens/home/auth_screen.dart';
 import '../../screens/splash/splash_screen.dart';
 
 @immutable
 class AppRouter {
   static Map<String, Widget Function(BuildContext)> routes() => {
         SplashScreen.routeName: (context) => const SplashScreen(),
+        AuthScreen.routeName: (context) => const AuthScreen(),
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        RegisterScreen.routeName: (context) => const RegisterScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
       };
 
   const AppRouter._();
