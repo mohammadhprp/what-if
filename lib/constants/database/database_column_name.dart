cd4129252c5deac46572ab40ad9b34bd0a39e7bd
Mohammad <mohammadhprp@gmail.com>
2023-07-10 16:25:03 +0330

    feat: add cloud database columns name

---
+++ lib/constants/database/database_column_name.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class DatabaseColumnName {
+  static const userId = 'id';
+  static const userName = 'user_name';
+  static const email = 'email';
+  static const profile = 'profile';
+}
