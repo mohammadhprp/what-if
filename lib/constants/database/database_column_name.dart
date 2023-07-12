a1eeefab12bc7704a44948379b70dcdc852dac85
Mohammad <mohammadhprp@gmail.com>
2023-07-12 14:57:11 +0330

    feat: add user profile key names

--- lib/constants/database/database_column_name.dart
+++ lib/constants/database/database_column_name.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart' show immutable;
 class DatabaseColumnName {
   static const id = 'id';
   static const userId = 'user_id';
+  static const profileId = 'profile_id';
   static const userName = 'user_name';
   static const name = 'name';
   static const email = 'email';
--- lib/constants/database/local_storage_name.dart
+++ lib/constants/database/local_storage_name.dart
@@ -5,6 +5,7 @@ class LocalStorageName {
   static const token = 'token';
   static const refreshToken = 'refresh_token';
   static const userId = 'user_id';
+  static const userProfile = 'user_profile';
   static const userName = 'user_name';
   static const email = 'email';
 
