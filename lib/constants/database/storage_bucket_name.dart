6df10b3ff86464e1662d06b123e9ff9641584e78
Mohammad <mohammadhprp@gmail.com>
2023-07-13 18:07:39 +0330

    feat: create a class for storage bucket names

---
+++ lib/constants/database/storage_bucket_name.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class StorageBucketName {
+  static const userProfileImages = 'user_profile_images';
+
+  const StorageBucketName._();
+}
