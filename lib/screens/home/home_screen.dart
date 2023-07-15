71d93936479d8bd6a5a8a0e18b2588073e117cb3
Mohammad <mohammadhprp@gmail.com>
2023-07-15 14:59:30 +0330

    feat: add Stories to Home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/extensions/theme/theme_extension.dart';
+import '../../constants/values_manager/values_manager.dart';
+import '../../helpers/localization/app_local.dart';
+import '../../widgets/home/stories/stories.dart';
+
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: context.theme.colorScheme.outlineVariant,
+        title: Text(
+          AppLocal.tr(context, 'app.what_if'),
+          style: context.textTheme.displayLarge,
+        ),
+        toolbarHeight: AppSize.s80,
+      ),
+      body: ListView(
+        children: const [
+          Stories(),
+        ],
       ),
     );
   }
