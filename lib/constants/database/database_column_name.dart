54aa19c827ced6ebaa3ac8c45df4d7d9acf191ba
Mohammad <mohammadhprp@gmail.com>
2023-07-15 18:47:06 +0330

    feat: add post column names

--- lib/constants/database/database_column_name.dart
+++ lib/constants/database/database_column_name.dart
@@ -3,12 +3,17 @@ import 'package:flutter/foundation.dart' show immutable;
 @immutable
 class DatabaseColumnName {
   static const id = 'id';
+  static const user = 'user';
   static const userId = 'user_id';
   static const profileId = 'profile_id';
   static const userName = 'user_name';
   static const name = 'name';
   static const email = 'email';
   static const image = 'image';
+  static const prompt = 'prompt';
+  static const caption = 'caption';
+  static const likeCount = 'like_count';
+  static const commentCount = 'comment_count';
   static const createdAt = 'created_at';
 
   const DatabaseColumnName._();
