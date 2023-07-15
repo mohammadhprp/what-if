3f8eb2510b56ec4a96c4c9929ab301e7de1395b0
Mohammad <mohammadhprp@gmail.com>
2023-07-15 18:47:38 +0330

    feat: add users profile and posts directorier

--- lib/constants/database/local_directory_name.dart
+++ lib/constants/database/local_directory_name.dart
@@ -3,6 +3,8 @@ import 'package:flutter/foundation.dart' show immutable;
 @immutable
 class LocalDirectoryName {
   static const userProfile = 'user_profile';
+  static const usersProfile = 'users_profile';
+  static const posts = 'posts';
 
   const LocalDirectoryName._();
 }
