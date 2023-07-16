1a91f3502030702bfd976e965e5bf616f23a0932
Mohammad <mohammadhprp@gmail.com>
2023-07-16 14:56:22 +0330

    feat: design post view in timeline

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,6 +4,7 @@ import '../../constants/extensions/theme/theme_extension.dart';
 import '../../constants/values_manager/values_manager.dart';
 import '../../helpers/localization/app_local.dart';
 import '../../widgets/home/stories/stories.dart';
+import '../../widgets/home/timeline/posts/posts.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
@@ -15,14 +16,17 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: context.theme.colorScheme.outlineVariant,
         title: Text(
-          AppLocal.tr(context, 'app.what_if'),
-          style: context.textTheme.displayLarge,
+          AppLocal.tr(context, 'app.timeline'),
+          style: context.textTheme.displayLarge?.copyWith(
+            color: context.theme.colorScheme.onBackground,
+          ),
         ),
         toolbarHeight: AppSize.s80,
       ),
       body: ListView(
         children: const [
           Stories(),
+          Posts(),
         ],
       ),
     );
