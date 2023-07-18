0b281fc75770f16c4eac1f426d708f255439bcff
Mohammad <mohammadhprp@gmail.com>
2023-07-18 18:47:00 +0330

    feat: create a step to user write caption for their post

---
+++ lib/widgets/post/add_post/write_caption/write_post_caption.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../../components/text_field/text_box_field.dart';
+import '../../../../constants/enums/create_post_state.dart';
+import '../../../../helpers/localization/app_local.dart';
+import '../../../../state/providers/post_providers/create_post_provider.dart';
+import '../../../../state/providers/post_providers/create_post_state_provider.dart';
+
+class WritePostCaption extends HookConsumerWidget {
+  const WritePostCaption({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller =
+        useTextEditingController(text: 'Look how this cat is cute');
+
+    return Column(
+      children: [
+        TextBoxField(
+          controller: controller,
+          hint: AppLocal.tr(context, 'form.message'),
+          minLine: 4,
+          onFiledSubmitted: controller.text.isNotEmpty
+              ? (value) {
+                  // Update createPost state to next step
+                  final state = ref.read(createPostStateProvider.notifier);
+                  state.update(CreatePostState.review);
+
+                  // Add post caption
+                  final postProvider = ref.read(createPostProvider.notifier);
+                  final post = ref.read(createPostProvider);
+                  postProvider.update(
+                    post?.copyWithCaption(value),
+                  );
+                }
+              : null,
+        ),
+      ],
+    );
+  }
+}
