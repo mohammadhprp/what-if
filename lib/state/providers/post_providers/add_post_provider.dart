91e855fde9be62efe25eaddf97183b363409e008
Mohammad <mohammadhprp@gmail.com>
2023-07-20 19:03:34 +0330

    feat: post user generated post

---
+++ lib/state/providers/post_providers/add_post_provider.dart
@@ -0,0 +1,7 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../notifiers/post_notifiers/add_post_notifier.dart';
+
+final addPostProvider = ChangeNotifierProvider<AddPostNotifier>(
+  (ref) => AddPostNotifier(),
+);
