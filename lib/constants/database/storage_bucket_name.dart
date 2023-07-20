d2e6abea916dff95150fb9b26f44aba0d0e5f4b2
Mohammad <mohammadhprp@gmail.com>
2023-07-20 19:02:23 +0330

    feat: add post table name

--- lib/constants/database/storage_bucket_name.dart
+++ lib/constants/database/storage_bucket_name.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart' show immutable;
 @immutable
 class StorageBucketName {
   static const userProfileImages = 'user_profile_images';
+  static const postImages = 'post_images';
 
   const StorageBucketName._();
 }
