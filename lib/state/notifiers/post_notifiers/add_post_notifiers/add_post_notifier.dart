89be12718ff1d27c16b6c55006354abe155dc059
Mohammad <mohammadhprp@gmail.com>
2023-07-23 17:13:32 +0330

    feat: add uploaded post to posts list in timeline

--- lib/state/notifiers/post_notifiers/add_post_notifiers/add_post_notifier.dart
+++ lib/state/notifiers/post_notifiers/add_post_notifiers/add_post_notifier.dart
@@ -6,34 +6,65 @@ import '../../../../constants/database/database_table_name.dart';
 import '../../../../constants/database/local_storage_name.dart';
 import '../../../../constants/database/storage_bucket_name.dart';
 import '../../../../constants/extensions/file/get_file.dart';
+import '../../../../constants/extensions/logger/logger_extension.dart';
 import '../../../../helpers/storage/local_storage.dart' as lg;
 import '../../../../models/post/create_post_model.dart';
+import '../../../../models/post/post_model.dart';
+import '../../../../utils/exceptions/message_exception.dart';
+import '../../../../utils/storage/user_info.dart';
 
 class AddPostNotifier extends ChangeNotifier {
-  Future<void> store(CreatePostModel post) async {
+  Future<PostModel> store(CreatePostModel post) async {
     // create a post
-    final supabase = Supabase.instance.client;
+    try {
+      final supabase = Supabase.instance.client;
 
-    final userId = await lg.LocalStorage.get(key: LocalStorageName.userId);
+      final userId = await lg.LocalStorage.get(key: LocalStorageName.userId);
 
-    final image = post.image!;
+      final image = post.image!;
 
-    // Upload post image
-    final path = await supabase.storage
-        .from(StorageBucketName.postImages)
-        .upload('$userId/${image.getFileName}', image);
+      // Upload post image
+      final path = await supabase.storage
+          .from(StorageBucketName.postImages)
+          .upload('$userId/${image.getFileName}', image);
 
-    // Insert post data
-    Map<String, dynamic> fields = {
-      DatabaseColumnName.userId: userId,
-      DatabaseColumnName.prompt: post.prompt,
-      DatabaseColumnName.caption: post.caption,
-      DatabaseColumnName.image: path,
-    };
+      // Insert post data
+      Map<String, dynamic> fields = {
+        DatabaseColumnName.userId: userId,
+        DatabaseColumnName.prompt: post.prompt,
+        DatabaseColumnName.caption: post.caption,
+        DatabaseColumnName.image: path,
+      };
 
-    await supabase.from(DatabaseTableName.posts).insert(fields);
+      final Map<String, dynamic> response = await supabase
+          .from(DatabaseTableName.posts)
+          .insert(fields)
+          .select()
+          .single();
 
-    // Delete the generated image from device
-    image.delete();
+      // Delete the generated image from device
+      image.delete();
+
+      /// Replace user_id to user_profiles
+      // Convert the map into a list
+      List<MapEntry<String, dynamic>> responseList = response.entries.toList();
+      // Find and re  move the entry with the key "user_id"
+      responseList
+          .removeWhere((entry) => entry.key == DatabaseColumnName.userId);
+      final currentUserProfile = await UserInfo.profile();
+
+      // Add a new entry with the key "user_profiles"
+      responseList.add(MapEntry(
+        DatabaseTableName.userProfiles,
+        currentUserProfile.toJson(),
+      ));
+      // Convert the list back into a map
+      final modifiedResponse = Map.fromEntries(responseList);
+
+      return PostModel.fromJson(modifiedResponse);
+    } on Exception catch (e) {
+      e.eLog();
+      throw MessageException('error.failed_to_add_post');
+    }
   }
 }
--- lib/state/notifiers/post_notifiers/post_list_notifiers/post_list_notifier.dart
+++ lib/state/notifiers/post_notifiers/post_list_notifiers/post_list_notifier.dart
@@ -6,6 +6,7 @@ import '../../../../constants/database/database_table_name.dart';
 import '../../../../constants/extensions/logger/logger_extension.dart';
 import '../../../../constants/typedefs.dart';
 import '../../../../models/post/post_model.dart';
+import '../../../../utils/exceptions/message_exception.dart';
 
 class PostListNotifier extends ChangeNotifier {
   IsLoading _isLoading = false;
@@ -49,9 +50,22 @@ class PostListNotifier extends ChangeNotifier {
       _list = loaded;
     } catch (e) {
       e.eLog();
+
+      throw MessageException('error.failed_to_fetch_posts');
     } finally {
       _isLoading = false;
       notifyListeners();
+    }
+  }
+
+  Future<void> add(PostModel post) async {
+    try {
+      _list.insert(0, post);
+      notifyListeners();
+    } catch (e) {
+      e.eLog();
+
+      throw MessageException('error.failed_to_add_post_to_post_list');
     }
   }
 }
