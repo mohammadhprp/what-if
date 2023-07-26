29d0557d673b5365b1a9f3816b8a49e5b0a62a86
Mohammad <mohammadhprp@gmail.com>
2023-07-26 17:06:36 +0330

    feat: add story images's bucket name class

--- lib/constants/database/storage_bucket_name.dart
+++ lib/constants/database/storage_bucket_name.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart' show immutable;
 class StorageBucketName {
   static const userProfileImages = 'user_profile_images';
   static const postImages = 'post_images';
+  static const storyImages = 'story_images';
 
   const StorageBucketName._();
 }
