d952f90ad3a8cf973679899f771bc859011a0ad6
Mohammad <mohammadhprp@gmail.com>
2023-08-05 18:09:03 +0330

    feat: get current user followers and following count

--- lib/widgets/user_profile/user_follower.dart
+++ lib/widgets/user_profile/user_follower.dart
@@ -1,70 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../components/loading/loading_view.dart';
 import '../../constants/values_manager/font_manager.dart';
 import '../../constants/values_manager/values_manager.dart';
+import '../../state/providers/follow_providers/follow_provider.dart';
 
 class UserFollower extends HookConsumerWidget {
   const UserFollower({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Column(
-          children: [
-            Text(
-              '10',
-              style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-            const SizedBox(height: AppSize.s10),
-            Text(
-              'Posts',
-              style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                    fontWeight: FontWeightManager.regular,
-                    color: Theme.of(context).colorScheme.outline,
-                  ),
-            ),
-          ],
+        Expanded(
+          child: Column(
+            children: [
+              Text(
+                '10',
+                style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              const SizedBox(height: AppSize.s10),
+              Text(
+                'Posts',
+                style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                      fontWeight: FontWeightManager.regular,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+              ),
+            ],
+          ),
         ),
-        Column(
-          children: [
-            Text(
-              '100',
-              style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-            const SizedBox(height: AppSize.s10),
-            Text(
-              'Followers',
-              style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                    fontWeight: FontWeightManager.regular,
-                    color: Theme.of(context).colorScheme.outline,
-                  ),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Text(
-              '20k',
-              style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-            const SizedBox(height: AppSize.s10),
-            Text(
-              'Following',
-              style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                    fontWeight: FontWeightManager.regular,
-                    color: Theme.of(context).colorScheme.outline,
-                  ),
-            ),
-          ],
-        ),
+        Expanded(
+          flex: 2,
+          child: Consumer(
+            builder: (context, ref, child) {
+              final myFollow = ref.watch(myFollowProvider);
+              return myFollow.when(
+                data: (follow) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            '${follow.follower}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge
+                                ?.copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onBackground,
+                                ),
+                          ),
+                          const SizedBox(height: AppSize.s10),
+                          Text(
+                            'Followers',
+                            style: Theme.of(context)
+                                .textTheme
+                                .displaySmall
+                                ?.copyWith(
+                                  fontWeight: FontWeightManager.regular,
+                                  color: Theme.of(context).colorScheme.outline,
+                                ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            '${follow.following}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge
+                                ?.copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onBackground,
+                                ),
+                          ),
+                          const SizedBox(height: AppSize.s10),
+                          Text(
+                            'Following',
+                            style: Theme.of(context)
+                                .textTheme
+                                .displaySmall
+                                ?.copyWith(
+                                  fontWeight: FontWeightManager.regular,
+                                  color: Theme.of(context).colorScheme.outline,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                },
+                error: (error, __) {
+                  return const Text('error');
+                },
+                loading: () => const LoadingView(),
+              );
+            },
+          ),
+        )
       ],
     );
   }
